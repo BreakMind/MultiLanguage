19696e8f274b2c08f6c26ee58817de391718e818
mkok <mustafa.kok@metus.com>
2017-03-06 14:56:03 +0200

    Added, sample WinForms application.

---
+++ SampleApplications/WinFormsSample/FrmMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NthDeveloper.MultiLanguage;
+
+namespace WinFormsSample
+{
+    public partial class FrmMain : Form
+    {
+        IMultiLanguageProvider m_LanguageProvider;
+
+        public FrmMain(IMultiLanguageProvider languageProvider)
+        {
+            InitializeComponent();
+
+            m_LanguageProvider = languageProvider;
+
+            translateUI();
+        }
+
+        private void translateUI()
+        {
+            this.Text = m_LanguageProvider.GetString("AppTitle");
+        }
+    }
+}
---
+++ SampleApplications/WinFormsSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using NthDeveloper.MultiLanguage;
+
+namespace WinFormsSample
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string _xmlLangugeFilesFolder = Path.GetFullPath(@"..\..\SampleData\XML");
+
+
+            XmlFileSource _xmlfileSource = new XmlFileSource(_xmlLangugeFilesFolder);
+            MultiLanguageProvider _languageProvider = new MultiLanguageProvider(_xmlfileSource);
+            _languageProvider.SetCurrentLanguage("en-US");
+
+            Application.Run(new FrmMain(_languageProvider));
+        }
+    }
+}
